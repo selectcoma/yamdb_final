809c6e89cca868372872f842f8e649d8764ec5d2
selectcoma <selectkoma@gmail.com>
2021-12-02 00:37:26 +0100

    first

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,12 +8,15 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+)
 
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default='*')]
 
 
 INSTALLED_APPS = [
